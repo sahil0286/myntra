8964f7b0d85424307c61c13409468f0777e6d15e
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-06-05 11:23:10 +0530

    Create UnderConstruction.js

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UnderConstructionImg from "../media/Website-under-construction.gif"
+
+const UnderConstruction = () => {
+  return (
+    <>
+    <div className='justify-content-center'>
+        <img className='justify-content-center' style={{hight:"100px" , width:"100px"}} src={UnderConstructionImg} alt="UnderConstructionImg" />
+    </div>
+    </>
+  )
+}
+
+export default UnderConstruction
