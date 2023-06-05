aeb29900d4461f68a32263e9ea1550de8109a85c
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-06-05 22:01:22 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,41 @@
 import React from 'react'
 import logo from "../logo.svg"
 import "./Header.css"
+import { FaRegUser } from "react-icons/fa"
+import { HiOutlineShoppingBag } from "react-icons/hi"
+import { AiOutlineHeart } from "react-icons/ai"
 
 const Header = () => {
   return (
     <>
-    <header className="p-3 header-shadow">
-    <div className="container headerContainer">
-      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none headerLogo">
-          <img src={logo} alt="logo" />
-        </a>
-        <ul className="nav col-12 col-lg-auto me-lg-auto mb-md-0">
-          <li><a href="/" className="nav-link headerNameStyle">MEN</a></li>
-          <li><a href="/" className="nav-link headerNameStyle">WOMEN</a></li>
-          <li><a href="/" className="nav-link headerNameStyle">KIDS</a></li>
-          <li><a href="/" className="nav-link headerNameStyle">HOME & LIVING</a></li>
-          <li><a href="/" className="nav-link headerNameStyle">BEAUTY</a></li>
-          <li><a href="/" className="nav-link headerNameStyle">STUDIO <sup className='textSupNew'>NEW</sup></a></li>
-        </ul>
-
-        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"/>
-        </form>
-
-        <div className="text-end">
-          <button type="button" className="btn btn-outline-light me-2">Login</button>
-          <button type="button" className="btn btn-warning">Sign-up</button>
-        </div>
-      </div>
-    </div>
-  </header>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary headerShadow">
+            <div className="container-fluid headerContainer">
+                <a className="navbar-brand d-flex align-items-center mb-2 mb-lg-0 text-decoration-none headerLogo" href="/">
+                    <img src={logo} alt="logo" />
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item"><a href="/" className="nav-link headerNameStyle">MEN</a></li>
+                    <li className="nav-item"><a href="/" className="nav-link headerNameStyle">WOMEN</a></li>
+                    <li className="nav-item"><a href="/" className="nav-link headerNameStyle">KIDS</a></li>
+                    <li className="nav-item"><a href="/" className="nav-link headerNameStyle">HOME & LIVING</a></li>
+                    <li className="nav-item"><a href="/" className="nav-link headerNameStyle">BEAUTY</a></li>
+                    <li className="nav-item"><a href="/" className="nav-link headerNameStyle">STUDIO <sup className='textSupNew'>NEW</sup></a></li>
+                </ul>
+                <form className="d-flex align-items-center" role="search">
+                    <input className="form-control me-2 searchBar" type="search" placeholder="🔍        Search for products, brands and more" aria-label="Search"/>
+                    <ul className="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+                        <li><a href="/" className="nav-link text-secondary mt-2"><FaRegUser className='bi d-block mx-auto' style={{fontSize:"18px"}}/><span className='headerIcon' style={{fontSize:"12px"}}>Profile</span></a></li>
+                        <li><a href="/" className="nav-link text-secondary mt-2"><AiOutlineHeart className='bi d-block mx-auto' style={{fontSize:"20px"}}/><span className='headerIcon'>Wishlist</span></a></li>
+                        <li><a href="/" className="nav-link text-secondary mt-2"><HiOutlineShoppingBag className='bi d-block mx-auto' style={{fontSize:"21px"}}/><span className='headerIcon'>Bag</span></a></li>
+                    </ul>
+                </form>
+                </div>
+            </div>
+        </nav>
     </>
   )
 }
