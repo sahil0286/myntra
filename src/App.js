84a80f59f7eb2213367faffff2fa910ab085e4dd
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-06-06 15:41:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import MainAd from './components/MainAd'
+import OmgDeal from './components/OmgDeal'
 
 const App = () => {
   return (
     <>
       <Header/>
+      <MainAd/>
+      <OmgDeal/>
     </>
   )
 }
