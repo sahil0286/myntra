924b2db6ceee56527ef2cf37e879b39db6aacbf5
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-06-10 18:48:35 +0530

    Update ProductPage.js

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -71,17 +71,17 @@ const ProductPage = () => {
               <span className='filterHeader'>CATEGORY</span>
                 <li style={{listStyleType: "none"}}>
                     <label>
-                        <input type="radio" name="category" value="" checked={category === ''} onChange={handleCategoryChange} />All Categories
+                        <input className="filterLabel"  type="radio" name="category" value="" checked={category === ''} onChange={handleCategoryChange} />All Category
                     </label>
                 </li>
                 <li style={{listStyleType: "none"}}>
                     <label>
-                        <input type="radio" name="category" value="men" checked={category === 'men'} onChange={handleCategoryChange} />Men
+                        <input className="filterLabel"  type="radio" name="category" value="men" checked={category === 'men'} onChange={handleCategoryChange} />Men
                 </label>
                 </li>
                 <li style={{listStyleType: "none"}}>
                     <label>
-                        <input type="radio" name="category" value="women" checked={category === 'women'} onChange={handleCategoryChange} />Women
+                        <input className="filterLabel"  type="radio" name="category" value="women" checked={category === 'women'} onChange={handleCategoryChange} />Women
                     </label>
                 </li>
             </div>
@@ -89,34 +89,44 @@ const ProductPage = () => {
               <span className='filterHeader'>SUBCATEGORY</span>
               {category === 'men' && (
                 <>
+                <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="subcategory" value="" checked={subcategory === ''} onChange={handleSubcategoryChange} />
-                    All Subcategories
+                    <input className="filterLabel"  type="radio" name="subcategory" value="" checked={subcategory === ''} onChange={handleSubcategoryChange} />All Subcategory
                   </label>
+                </li>
+                <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="subcategory" value="tshirts" checked={subcategory === 'tshirts'} onChange={handleSubcategoryChange} />
-                    T-Shirts
+                    <input className="filterLabel"  type="radio" name="subcategory" value="tshirts" checked={subcategory === 'tshirts'} onChange={handleSubcategoryChange} />T-Shirts
                   </label>
+                </li>
+                <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="subcategory" value="long tshirts" checked={subcategory === 'long tshirts'} onChange={handleSubcategoryChange} />
+                    <input className="filterLabel"  type="radio" name="subcategory" value="long tshirts" checked={subcategory === 'long tshirts'} onChange={handleSubcategoryChange} />
                     Long T-Shirts
                   </label>
+                </li>
                 </>
               )}
               {category === 'women' && (
                 <>
+                <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="subcategory" value="" checked={subcategory === ''} onChange={handleSubcategoryChange} />
+                    <input className="filterLabel"  type="radio" name="subcategory" value="" checked={subcategory === ''} onChange={handleSubcategoryChange} />
                     All Subcategories
                   </label>
+                </li>
+                  <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="subcategory" value="kurta set" checked={subcategory === 'kurta set'} onChange={handleSubcategoryChange} />
+                    <input className="filterLabel"  type="radio" name="subcategory" value="kurta set" checked={subcategory === 'kurta set'} onChange={handleSubcategoryChange} />
                     Kurta Set
                   </label>
+                  </li>
+                  <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="subcategory" value="kurta" checked={subcategory === 'kurta'} onChange={handleSubcategoryChange} />
+                    <input className="filterLabel" type="radio" name="subcategory" value="kurta" checked={subcategory === 'kurta'} onChange={handleSubcategoryChange} />
                     Kurta
                   </label>
+                  </li>
                 </>
               )}
             </div>
@@ -124,32 +134,44 @@ const ProductPage = () => {
               <span className='filterHeader'>BRAND</span>
               {category !== '' && (
                 <>
+                <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="brand" value="" checked={brand === ''} onChange={handleBrandChange} />
+                    <input className="filterLabel" type="radio" name="brand" value="" checked={brand === ''} onChange={handleBrandChange} />
                     All Brands
                   </label>
+                </li>
                   {category === 'men' && (
                     <>
+                    <li style={{listStyleType: "none"}}>
+
                       <label>
-                        <input type="radio" name="brand" value="Rx" checked={brand === 'Rx'} onChange={handleBrandChange} />
+                        <input className="filterLabel" type="radio" name="brand" value="Rx" checked={brand === 'Rx'} onChange={handleBrandChange} />
                         Rx
                       </label>
+                    </li>
+                      <li style={{listStyleType: "none"}}>
+
                       <label>
-                        <input type="radio" name="brand" value="Ry" checked={brand === 'Ry'} onChange={handleBrandChange} />
+                        <input className="filterLabel" type="radio" name="brand" value="Ry" checked={brand === 'Ry'} onChange={handleBrandChange} />
                         Ry
                       </label>
+                      </li>
                     </>
                   )}
                   {category === 'women' && (
                     <>
+                    <li style={{listStyleType: "none"}}>
                       <label>
-                        <input type="radio" name="brand" value="Wx" checked={brand === 'Wx'} onChange={handleBrandChange} />
+                        <input className="filterLabel" type="radio" name="brand" value="Wx" checked={brand === 'Wx'} onChange={handleBrandChange} />
                         Wx
                       </label>
+                    </li>
+                      <li style={{listStyleType: "none"}}>
                       <label>
-                        <input type="radio" name="brand" value="Wy" checked={brand === 'Wy'} onChange={handleBrandChange} />
+                        <input className="filterLabel" type="radio" name="brand" value="Wy" checked={brand === 'Wy'} onChange={handleBrandChange} />
                         Wy
                       </label>
+                      </li>
                     </>
                   )}
                 </>
@@ -159,65 +181,83 @@ const ProductPage = () => {
               <span className='filterHeader'>COLOR</span>
               {category === 'men' && (
                 <>
+                <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="color" value="" checked={color === ''} onChange={handleColorChange} />
+                    <input className="filterLabel" type="radio" name="color" value="" checked={color === ''} onChange={handleColorChange} />
                     All Colors
                   </label>
+                </li>
+                  <li  style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="color" value="red" checked={color === 'red'} onChange={handleColorChange} />
+                    <input className="filterLabel" type="radio" name="color" value="red" checked={color === 'red'} onChange={handleColorChange} />
                     Red
                   </label>
+                  </li>
+                  <li  style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="color" value="green" checked={color === 'green'} onChange={handleColorChange} />
+                    <input className="filterLabel" type="radio" name="color" value="green" checked={color === 'green'} onChange={handleColorChange} />
                     Green
                   </label>
+                  </li>
+                  <li  style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="color" value="black" checked={color === 'black'} onChange={handleColorChange} />
+                    <input className="filterLabel" type="radio" name="color" value="black" checked={color === 'black'} onChange={handleColorChange} />
                     Black
                   </label>
+                  </li>
                 </>
-              )}
+               )}
               {category === 'women' && (
                 <>
+                <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="color" value="" checked={color === ''} onChange={handleColorChange} />
+                    <input className="filterLabel" type="radio" name="color" value="" checked={color === ''} onChange={handleColorChange} />
                     All Colors
                   </label>
+                </li>
+                  <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="color" value="gold" checked={color === 'gold'} onChange={handleColorChange} />
+                    <input className="filterLabel" type="radio" name="color" value="gold" checked={color === 'gold'} onChange={handleColorChange} />
                     Gold
                   </label>
+                  </li>
+                  <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="color" value="yellow" checked={color === 'yellow'} onChange={handleColorChange} />
+                    <input className="filterLabel" type="radio" name="color" value="yellow" checked={color === 'yellow'} onChange={handleColorChange} />
                     Yellow
                   </label>
+                  </li>
+                  <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="color" value="green" checked={color === 'green'} onChange={handleColorChange} />
+                    <input className="filterLabel" type="radio" name="color" value="green" checked={color === 'green'} onChange={handleColorChange} />
                     Green
                   </label>
+                  </li>
+                  <li style={{listStyleType: "none"}}>
                   <label>
-                    <input type="radio" name="color" value="pink" checked={color === 'pink'} onChange={handleColorChange} />
+                    <input className="filterLabel" type="radio" name="color" value="pink" checked={color === 'pink'} onChange={handleColorChange} />
                     Pink
                   </label>
+                  </li>
                 </>
-              )}
+                )}
             </div>
             <div className="filter-option">
               <span className='filterHeader'>MAX PRICE</span>
-              <input type="text" placeholder="Max Price" value={price} onChange={handlePriceChange} />
+              <input type="text" style={{borderRadius:"2px",marginLeft:"0px"}} placeholder="Max Price" value={price} onChange={handlePriceChange} />
             </div>
             <div className="filter-option">
               <span className='filterHeader'>DISCOUNT</span>
-              <select value={discount} onChange={handleDiscountChange}>
+              <select value={discount} style={{borderRadius:"2px",marginLeft:"0px"}} onChange={handleDiscountChange}>
                 <option value="">All Discounts</option>
-                <option value="10">10% off</option>
-                <option value="20">20% off</option>
-                <option value="30">30% off</option>
-                <option value="40">40% off</option>
-                <option value="50">50% off</option>
-                <option value="60">60% off</option>
-                <option value="70">70% off</option>
-                <option value="80">80% off</option>
+                <option value="10">10% and above</option>
+                <option value="20">20% and above</option>
+                <option value="30">30% and above</option>
+                <option value="40">40% and above</option>
+                <option value="50">50% and above</option>
+                <option value="60">60% and above</option>
+                <option value="70">70% and above</option>
+                <option value="80">80% and above</option>
               </select>
             </div>
             <button  onClick={handleResetFilters} style={{borderRadius:"20px"}}>Reset Filters</button>
