ef41bb7ffe83fc2f7b5c31032ddc4eb652c6ca76
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-06-12 18:19:39 +0530

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,6 @@ const Product = (prop) => {
         <img src={prop.image} className="card-img-top" alt={prop.image}/>
         <p className=" productName mt-2">{prop.name}</p>
         <p className="productDetail mt-2">{prop.subcategory} from {prop.brand} brand in {prop.color} Color</p>
-
         <p className="card-text mt-2 mainPrice">Rs. {prop.price-prop.price*(prop.discount)/100} <span className='discountedPrice'>Rs. {prop.price}</span><span className='discountRate' > ( {prop.discount}% OFF )</span></p>
     </>
   )
